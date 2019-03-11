fa974587b556f43532e9fc1d2425926900b926f1
Victor P. Unda <llajta9@gmail.com>
2019-03-11 13:30:21 -0700

    Adding sts.replace
    str.replace, more than 3.

--- script2.py
+++ script2.py
@@ -6,16 +6,25 @@
 # https://www.python.org/
 
 import csv          # Lib/csv.py ...file
-import itertools    #function for a efficient looping
+import itertools    #function for an efficient looping
+import codecs
+import string
 
-with open('Abiutxt.txt', 'r') as in_file:
-    lines = in_file.read().splitlines()     #returns a list with all the lines in string, including the line breaks
+with open("Apple.txt", "r") as in_file:
 
-    stripped = [line.split('. ', 1)for line in lines ] #split, first "."
+    linee = str(in_file.read())
+    cropline = str.replace(linee, 'Origin:', '<b>Origin:</b>')
+    cropline2 = str.replace(cropline, 'Fruit:', '<b>Fruit:</b>')
+    cropline3 = str.replace(cropline2, 'Tree:', '<b>Tree:</b>')
+
+    lines = cropline3.splitlines()     #returns a list with all the lines in string, including the line breaks
+
+    stripped = [line.split('. ', 1)for line in lines ] #split, first ". "
 
     grouped = itertools.izip(*[stripped]*1) #Functions creating iterators for efficient (izip) looping
 
-    with open('AbiutCSS.csv', 'w') as out_file: # this is my output file
+    with open('Apple01.csv', 'w') as out_file: # this is my output file
+
         nrsp10_w = csv.writer(out_file) # return object responsible for converting nrsp10 data into delimited strings.
         nrsp10_w.writerow(('Crop', 'Description')) # write the row paremeter to the writer's nrsp10 data file object.
 
